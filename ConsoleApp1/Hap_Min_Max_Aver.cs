7fde517a022b5ca623e48132a9ac5d625965a6b5
heoeuntaek <gjdmsxor@naver.com>
2022-09-27 14:29:09 +0900

    d

--- ConsoleApp1/Hap_Min_Max_Aver.cs
+++ ConsoleApp1/Hap_Min_Max_Aver.cs
@@ -1,65 +1,72 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConsoleApp1
-{
-    internal class Hap_Min_Max_Aver
-    {
-        static void Main(string[] args)
-        {
-            int max = 0;
-            int min = 1000;
-            int hap = 0;
-            int ave = 0;
-
-            int[] su = new int[] { 1, 3 ,4 , 2,3,10 } ;
-            Console.WriteLine(su.Length);
-            
-            for(int i = 0; i < su.Length; i++)
-            {
-                hap+=su[i];
-            }
-            //합
-            //    최대
-            //    최소
-            //    평균
-            max = su[0];
-
-            for(int i=1; i <su.Length; i++)
-            {
-                if (max < su[i])
-                {
-                    max = su[i];
-                }
-            }
-
-            min = su[0];
-            for(int i=1; i < su.Length; i++)
-            {
-                if(min > su[i])
-                {
-                    min = su[i];
-                }
-            }
-
-            ave = hap / su.Length;
-
-            Console.WriteLine("Hap ="+hap);
-            Console.WriteLine("min =" + min);
-            Console.WriteLine("max =" + max);
-            Console.WriteLine("ave =" + ave);
-
-            
-            Console.WriteLine("ave =" + ave);
-            Console.WriteLine("ave =" + ave);
-            Console.WriteLine("ave =" + ave);
-            
-
-
-
-        }
-    }
-}
+﻿// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Text;
+// using System.Threading.Tasks;
+//
+// namespace ConsoleApp1
+// {
+//     internal class Hap_Min_Max_Aver
+//     {
+//         public Hap_Min_Max_Aver()
+//         {
+//         }
+//
+//         static void Main(string[] args)
+//         {
+//             int max = 0;
+//             int min = 1000;
+//             int hap = 0;
+//             int ave = 0;
+//
+//             int[] su = new int[] { 1, 3 ,4 , 2,3,10 } ;
+//             Console.WriteLine(su.Length);
+//             
+//             for(int i = 0; i < su.Length; i++)
+//             {
+//                 hap+=su[i];
+//             }
+//             //합
+//             //    최대
+//             //    최소
+//             //    평균
+//             max = su[0];
+//
+//             for(int i=1; i <su.Length; i++)
+//             {
+//                 if (max < su[i])
+//                 {
+//                     max = su[i];
+//                 }
+//             }
+//
+//             min = su[0];
+//             for(int i=1; i < su.Length; i++)
+//             {
+//                 if(min > su[i])
+//                 {
+//                     min = su[i];
+//                 }
+//             }
+//
+//             ave = hap / su.Length;
+//
+//             Console.WriteLine("Hap ="+hap);
+//             Console.WriteLine("min =" + min);
+//             Console.WriteLine("max =" + max);
+//             Console.WriteLine("ave =" + ave);
+//
+//             
+//             Console.WriteLine("ave =" + ave);
+//             Console.WriteLine("ave =" + ave);
+//             Console.WriteLine("ave =" + ave);
+//             
+//             
+//             
+//             
+//
+//
+//
+//         }
+//     }
+// }
---
+++ ConsoleApp1/Search.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp1;
+
+public class Search
+{
+    static void Main(string[] args)
+    {
+        int data = 0;
+        int[] su = new int[] { 1, 3, 5, 7, 9, 2, 4, 3, 7 };
+        Console.WriteLine("1부터 10까지 사이 입력");
+        data = Convert.ToInt32(Console.ReadLine());
+        
+        int []a = new []{1,2,3,4,5
+
+        Console.WriteLine("찾는 데이터는 {} 은 {}번째 인덱스에 있습니다.",i,);
+    }
+}
